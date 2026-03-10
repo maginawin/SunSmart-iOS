5ce736de48aefa1cd0d5c3abc55a9610e2116037
ykehong <15920567464@163.com>
2026-03-10 16:39:43 +0800

    组pir禁用修改

--- SunSmart/Main/Group/Controller/GroupViewController.swift
+++ SunSmart/Main/Group/Controller/GroupViewController.swift
@@ -1453,10 +1453,10 @@ extension GroupViewController: GroupSensorViewDelegate {
             sensor.occupancySettings = false
             switch result {
             case .success:
-                sensor.pirEnabled = enable
-                if let meshUUID = sensor.network?.uuid.uuidString {
-                    sensor.preConfiguration.save(meshUUID: meshUUID, nodeAddress: sensor.primaryUnicastAddress)
-                }
+//                sensor.pirEnabled = enable
+//                if let meshUUID = sensor.network?.uuid.uuidString {
+//                    sensor.preConfiguration.save(meshUUID: meshUUID, nodeAddress: sensor.primaryUnicastAddress)
+//                }
                 ToastStatusView.show(in: self.view, message: "configuration_successful".localizedString, type: .success)
                 // 同步数据到服务器
                 NotificationCenter.default.post(name: .init(spaceDataChangedNotificaitonName), object: SpaceChangeDataType.device)
