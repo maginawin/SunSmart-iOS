8dddc415ad550c3463dbe6db5cc1d83d96542845
PlatoJobs <platojobs@qq.com>
2026-04-08 10:11:40 +0800

    MacroDefinition 适配层的修改

--- SunSmart/Common/Macro/MacroDefinition.swift
+++ SunSmart/Common/Macro/MacroDefinition.swift
@@ -7,15 +7,20 @@
 
 import UIKit
 
-/** 屏幕宽度*/
-let SCREEN_WIDTH = UIScreen.main.bounds.size.width
-/** 屏幕高度*/
-let SCREEN_HEIGHT = UIScreen.main.bounds.size.height
+var SCREEN_WIDTH: CGFloat {
+    UIScreen.main.bounds.size.width
+}
+
+var SCREEN_HEIGHT: CGFloat {
+    UIScreen.main.bounds.size.height
+}
 /** 设备是否为iPhone X*/
 //if #available(iOS 13.0, *) {
 
 //} else {
-let isIphoneX = StatusBarManager.statusBarFrame.size.height >= 44
+var isIphoneX: Bool {
+    kSafeAreaTopHeight >= 44
+}
 //}
 /** 导航条高度*/
 //let kNavigationHeight = StatusBarManager.statusBarFrame.size.height + 44
@@ -25,7 +30,7 @@ let kNavigationHeight = kSafeAreaTopHeight + 44
 let kTabbarHeight = CGFloat(kSafeAreaBottomHeight + 44)
 
 /// 导航栏右侧item边距
-let navigationRightItemMargin = isIPad ? 20 : SCRXFrom(SCRXFrom(15.5))
+let navigationRightItemMargin = isIPad ? 20 : (SCRXFrom(15.5))
 
 /// 窗口边距
 var kWindowSafeAreaInsets: UIEdgeInsets {
@@ -38,15 +43,23 @@ var kWindowSafeAreaInsets: UIEdgeInsets {
 }
 
 /// 顶部导航栏边距
-let kSafeAreaTopHeight = kWindowSafeAreaInsets.top
+var kSafeAreaTopHeight: CGFloat {
+    kWindowSafeAreaInsets.top
+}
 
 //let kSafeAreaTopHeight = StatusBarManager.statusBarFrame.size.height
 /** 底部边界值  iPhone X类型使用*/
-let kSafeAreaBottomHeight = kWindowSafeAreaInsets.bottom
+var kSafeAreaBottomHeight: CGFloat {
+    kWindowSafeAreaInsets.bottom
+}
 //CGFloat(isIphoneX ? 34 : 0)
-//if #available(iOS 13.0, *) {
-let StatusBarManager = UIApplication.shared.windows.first!.windowScene!.statusBarManager!
-//}
+
+var StatusBarManager: UIStatusBarManager? {
+    UIApplication.shared.connectedScenes
+        .compactMap { $0 as? UIWindowScene }
+        .first?.statusBarManager
+}
+
 
 /// iPhone手机UI基准尺寸
 let iPhoneStandardSize = CGSize(width: 375.0, height: 812.0)
@@ -65,17 +78,7 @@ func SCRXFrom(_ x : CGFloat) -> CGFloat {
 
 // 屏幕适配 (y)
 func SCRYFrom(_ y : CGFloat) -> CGFloat {
-    if isIPad {
-        guard kSafeAreaBottomHeight > 0 else {
-            return y
-        }
-        return y * SCREEN_HEIGHT / iPadStandardSize.height
-    }
-    guard isIphoneX else {
-        return y
-    }
-    return y * SCREEN_HEIGHT / iPhoneStandardSize.height
-//    (isIphoneX ? 736 : SCREEN_HEIGHT) / 667.0 * y
+    return SCRYFit(y)
 }
 
 func FontFit(_ size: CGFloat) -> CGFloat {
@@ -86,14 +89,19 @@ func FontFit(_ size: CGFloat) -> CGFloat {
     return size * min(scale, 1.2)
 }
 
-// 屏幕等比适配 (y)
+// 屏幕适配 (y)
 func SCRYFit(_ y : CGFloat) -> CGFloat {
     let standard: CGFloat = isIPad ? iPadStandardSize.height : iPhoneStandardSize.height
     return SCREEN_HEIGHT / standard * y
 }
 
-let isIPad = UIDevice.current.model == "iPad"
-let isIPhone = UIDevice.current.model == "iPhone"
+var isIPad: Bool {
+    UIDevice.current.userInterfaceIdiom == .pad
+}
+
+var isIPhone: Bool {
+    UIDevice.current.userInterfaceIdiom == .phone
+}
 
 // 检查当前首选语言是否是中文
 let isChineseLanguage = Locale.preferredLanguages.first?.hasPrefix("zh") ?? false
