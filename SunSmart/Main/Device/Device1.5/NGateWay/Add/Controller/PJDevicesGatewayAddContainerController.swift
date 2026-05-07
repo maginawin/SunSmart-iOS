c0d99db0a364b2b76c72a09ca59850f58d79f98e
PlatoJobs <platojobs@qq.com>
2026-05-07 10:37:20 +0800

    网桥添加桥接文件

---
+++ SunSmart/Main/Device/Device1.5/NGateWay/Add/Controller/PJDevicesGatewayAddContainerController.swift
@@ -0,0 +1,32 @@
+//
+//  PJDevicesGatewayAddContainerController.swift
+//  SunSmart
+//
+//  Created by Plato Jobs.
+//
+
+import UIKit
+
+final class PJDevicesGatewayAddContainerController: PJDevicesLegacyContainerController {
+
+    private let context: PJDevicesAddEntryContext
+
+    init(context: PJDevicesAddEntryContext) {
+        self.context = context
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = context.title
+        let vc = DeviceAddViewController(space: context.space)
+        vc.title = context.title
+        vc.appointGroup = context.appointGroup
+        vc.forceBindToDongle = context.forceBindToDongle
+        embedLegacyController(vc)
+    }
+}
